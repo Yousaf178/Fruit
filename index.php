290c513557c44fc0146e7d6550affe6846fdb732
unknown <youahmad224@gmail.com>
2024-11-22 19:06:21 -0900

    changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Turbela fruits company</title>
+<title>Pakistan Fruits Company</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Eco Fruits Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
